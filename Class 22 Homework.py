df2453e69734f7b2822c7247ad58c345971e2cf0
rajgo <deepikamano20@gmail.com>
2023-12-17 21:06:54 -0500

    Filter/Map Function V2

---
+++ Class 22 Homework.py
@@ -0,0 +1,14 @@
+#Greatest of 3 Numbers
+'''print("Greatest of 3 numbers")
+a=235
+b=400
+c=600
+output=lambda a,b,c:f"The greatest of 3 numbers is {a}" if a>b and a>c else f"The greatest of 3 numbers is {b}" if b>a and b>c else f"The greatest of 3 numbers is {c}"
+print(output(600,300,200))'''
+#Mapping a List
+'''names=["karan","karvin" ,"poovin","saran","tharun" ,"saranya" ,"angela" ,"sanjith"]
+output=list(map(lambda a:len(a),names))
+print(output)'''
+#Filtering Names from a List
+'''output=list(filter(lambda a:len(a)==6,names))
+print(output)'''
