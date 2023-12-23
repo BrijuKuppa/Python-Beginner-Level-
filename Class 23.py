4f29793737084f8eef17eb68ad4e5fc0346950f8
rajgo <deepikamano20@gmail.com>
2023-12-22 19:58:33 -0500

    Graphical User Interface (GUI)

---
+++ Class 23.py
@@ -0,0 +1,7 @@
+#Graphical User Interface (GUI)
+from tkinter import *
+window=Tk()
+window.title("Graphical User Interface (GUI)")
+window.geometry("1000x1000")
+window.config(bg="#4CB9E7")
+window.mainloop()
