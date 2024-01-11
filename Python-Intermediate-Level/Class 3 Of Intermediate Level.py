a8dbfc77b75da1b6936d98d3ac2de4e7c4c14d32
rajgo <deepikamano20@gmail.com>
2024-01-10 20:35:55 -0500

    Photo Album Slideshow (GUI) V2

--- Python-Intermediate-Level/Class 3 Of Intermediate Level.py
+++ Python-Intermediate-Level/Class 3 Of Intermediate Level.py
@@ -18,8 +18,20 @@ image_list=[pic_1,pic_2,pic_3,pic_4,pic_5]
 index=0
 lb1=Label(window,image=image_list[index])
 lb1.place(x=50,y=50)
-
-def next():
+#With Slideshow
+lb2=Label(window,text="Picture Slideshow",font=("ariel",15,"underline"))
+lb2.pack()
+def slideshow():
+    global index
+    index=index+1
+    if index==5:
+        index=0
+    lb1=Label(window, image=image_list[index])
+    lb1.place(x=50, y=50)
+    lb1.after(5000,slideshow)
+slideshow()
+#With Buttons
+'''def next():
     global index
     index=index+1
     if index==5:
@@ -33,10 +45,9 @@ def back():
     if index==-6:
         index=-1
     lb1=Label(window, image=image_list[index])
-    lb1.place(x=50, y=50)
-
-btn1=Button(window,text="Next\n⏭️",command=next)
+    lb1.place(x=50, y=50)'''
+'''btn1=Button(window,text="Next\n⏭️",command=next)
 btn1.place(x=380,y=320)
 btn2=Button(window,text="Previous\n    ⏮️️",command=back)
-btn2.place(x=100,y=320)
+btn2.place(x=100,y=320)'''
 window.mainloop()
