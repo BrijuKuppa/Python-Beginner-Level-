8f309ebe1f9d5105fcf8918ed1601fb8105eb15d
rajgo <deepikamano20@gmail.com>
2024-01-15 15:55:57 -0500

    Homework

---
+++ Python-Intermediate-Level/Class 4 Homework.py
@@ -0,0 +1,14 @@
+#Practice Program-1
+'''bank_acn={"lim":12345, "sam":3456}
+print(bank_acn["lim"])'''
+#Practice Program-2
+'''bank_acn={"lim":12345, "sam":3456}
+bank_acn["caryn"]="999"
+print(bank_acn)'''
+#Practice Program-3
+'''bank_balance={"lim":5000,"sam":5000}
+bank_balance["lim"]=bank_balance["lim"]+2000
+print(bank_balance)'''
+#Practice Program-4
+'''names={"name":"caryn","grade":7,"place":"Malaysia"}
+print(names["place"])'''
