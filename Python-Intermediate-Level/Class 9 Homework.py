488cf3c00fe71863b41c39487bf35c689e9d62e5
rajgo <deepikamano20@gmail.com>
2024-02-05 20:48:28 -0500

    Rock Paper Scissors Homework

---
+++ Python-Intermediate-Level/Class 9 Homework.py
@@ -0,0 +1,32 @@
+#Practice Program-1
+'''country_capitals={"Germany":"Berlin","Canada":"Ottawa","England":"London"}
+print(f"The Capital of Germany is: {country_capitals['Germany']}")
+print(f"The capital of England is: {country_capitals['England']}")'''
+#Practice Program-2
+'''country_capitals={"Germany":"Berlin","Canada": "Ottawa"}
+country_capitals["Italy"]="Rome"
+print(country_capitals)'''
+#Practice Program-3
+
+#Pop()
+'''country_capitals={"Germany":"Berlin","Canada":"Ottawa","England":"London"}
+country_capitals.pop("England")
+print(country_capitals)'''
+
+#Update()
+'''country_capitals={"Germany":"Berlin","Canada":"Ottawa","England":"London"}
+country_capitals.update({'Italy':'Rome'})
+print(country_capitals)'''
+
+#Clear()
+'''country_capitals={"Germany":"Berlin","Canada":"Ottawa","England":"London"}
+country_capitals.clear()
+print(country_capitals)'''
+
+#Keys()
+'''country_capitals={"Germany":"Berlin","Canada":"Ottawa","England":"London"}
+print(country_capitals.keys())'''
+
+#Values
+'''country_capitals={"Germany":"Berlin","Canada":"Ottawa","England":"London"}
+print(country_capitals.values())'''
