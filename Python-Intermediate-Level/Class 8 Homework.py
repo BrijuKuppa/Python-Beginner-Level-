b5551c7a778d8b74a34fdb84aad66169cfb779ac
rajgo <deepikamano20@gmail.com>
2024-02-05 20:26:34 -0500

    Rock Paper Scissors Homework

---
+++ Python-Intermediate-Level/Class 8 Homework.py
@@ -0,0 +1,28 @@
+#Practice Program-1
+'''List=['Mathematics', 'chemistry', 1997, 2000]
+List.append(2023)
+print(List)'''
+#Practice Program-2
+'''List=['Mathematics', 'chemistry', 1997, 2000]
+print(f"The index number of {List[1]} is {List.index('chemistry')}")'''
+#Practice Program-3
+'''List=['Mathematics', 'chemistry', 1997, 2000]
+print(List[1])'''
+#Practice Program-4
+'''s="python"
+list=[]
+upper_list=[]
+for i in s:
+    list.append(i)
+for i in list:
+    upper_list.append(i.upper())
+print(list)
+print(upper_list)'''
+#Practice Program-5
+'''s_list=['P', 'Y', 'T', 'H', 'O', 'N']
+s_str=""
+for i in s_list:
+    s_str=s_str+i
+print(s_str.lower())
+print(s_str)
+print(s_str.title())'''
