f9c4c2b6794a29f93236c830be2ea6c4fdcb84b9
rajgo <deepikamano20@gmail.com>
2024-02-16 21:56:25 -0500

    Weather Appliaction

--- Python-Intermediate-Level/Class 10 of Intermediate Level.py
+++ Python-Intermediate-Level/Class 10 of Intermediate Level.py
@@ -18,8 +18,10 @@ place=Label(window,text="[place]",font=("courier",15),bg="#8CB9BD")
 place.place(x=190,y=280)
 label_weather=Label(window,text="Weather:",font=("courier",15,"underline"),bg="#8CB9BD")
 label_weather.place(x=187,y=320)
-label_temp=Label(window,text="[temprature]",font=("courier",15),bg="#8CB9BD")
+label_temp=Label(window,text="[temperature]",font=("courier",15),bg="#8CB9BD")
 label_temp.place(x=50,y=360)
+label_humidity=Label(window,text="Humidity: [humidity]",font=("courier",15),bg="#8CB9BD")
+label_humidity.place(x=50,y=400)
 api="e0b19566bba38f97d53743b5e55c8662"
 location=["cary","angier","charlotte","durham","mooresville"]
 
@@ -30,7 +32,9 @@ def weather():
     data=json.loads(response)
     temp=data["main"]["temp"]
     temp=round(temp-273.15,2)
-    label_temp.config(text=f"Temprature: {temp}")
+    humidity=data["main"]["humidity"]
+    label_temp.config(text=f"Temperature: {temp}")
+    label_humidity.config(text=f"Humidity: {humidity}")
     place.config(text=place2.title())
     place.after(1000,weather)
 weather()
