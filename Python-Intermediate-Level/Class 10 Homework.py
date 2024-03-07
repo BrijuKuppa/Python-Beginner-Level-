8057d5c0413164ee2860bcded91e643efec91ee8
rajgo <deepikamano20@gmail.com>
2024-03-06 19:52:16 -0500

    Homework

--- Python-Intermediate-Level/Class 10 Homework.py
+++ Python-Intermediate-Level/Class 10 Homework.py
@@ -7,17 +7,19 @@ output_odd=filter(lambda a: a%2==1,numbers)
 print(list(output_even))
 print(list(output_odd))'''
 #Practice Program-2
-numbers=[1, 2, 3, 5, 7, 8, 9, 10]
-even_numbers=0
-odd_numbers=0
-for i in numbers:
-    if i%2==0:
-        even_numbers=even_numbers+1
-    else:
-        odd_numbers=odd_numbers+1
-print(even_numbers)
-print(odd_numbers)
+'''numbers=[1, 2, 3, 5, 7, 8, 9, 10]
+even_numbers=list(filter(lambda a:a%2==0,numbers))
+odd_numbers=list(filter(lambda a:a%2!=0,numbers))
+even=len(even_numbers)
+odd=len(odd_numbers)
+print(f"Number of even numbers: {even}")
+print(f"Number of odd numbers: {odd}")'''
 #Practice Program-3
 '''weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-output=filter(lambda a: a==6,numbers)
+output=list(filter(lambda a: len(a)==6,weekdays))
+print(output)'''
+#Practice Program-4
+'''a=[1, 2, 3]
+b=[4, 5, 6]
+output=list(map(lambda c,d:c+d,a,b))
 print(output)'''
