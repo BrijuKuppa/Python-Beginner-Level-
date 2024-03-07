f8d07c5cde47c930e22deb40437769cd008e8b70
rajgo <deepikamano20@gmail.com>
2024-03-06 20:45:07 -0500

    Classwork

---
+++ Python-Intermediate-Level/Class 16 of Intermediate Level.py
@@ -0,0 +1,23 @@
+#Creating Classes and Objects
+'''class Test:
+    def __init__(self):
+        print("Constructor Created")
+
+ob1=Test()'''
+
+'''class Fruit:
+    def __init__(self,color,weight,taste,size,name):
+        self.color=color
+        self.weight=weight
+        self.taste=taste
+        self.size=size
+        self.name=name
+        self.place="USA"
+    def intro(self):
+        print(f"Fruit Information:\n Fruit name: {self.name}\n Fruit color: {self.color}\n Fruit weight: {self.weight}\n Fruit size: {self.size}\n Fruit taste: {self.taste}")
+    def __del__(self):
+        print("Self Destruct Complete")
+
+ob1=Fruit("red","150 grams","sweet","10 inches","apple")'''
+'''print(ob1.size,ob1.weight,ob1.place,ob1.taste,ob1.name)'''
+'''ob1.intro()'''
