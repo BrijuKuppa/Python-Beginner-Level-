9c54de11481c771ba7ffa73b9d38d6bcfbca4e38
rajgo <deepikamano20@gmail.com>
2024-03-10 13:19:41 -0400

    Homework

---
+++ Python-Intermediate-Level/Class 17 Homework.py
@@ -0,0 +1,19 @@
+#Practice Program-1: Type Casting
+'''a=1.34556
+print(str(a))'''
+#Practice Program-2: More Type Casting
+'''s="python"
+print(f"Variable 's' to a list: {list(s)}")
+print(f"Variable 's' to a tuple: {tuple(s)}")
+print(f"Variable 's' to a set: {set(s)}")'''
+#Practice Program-3: Union, Intersection, and Difference with Sets
+'''SET1={1, 2, 3, 4, 5, 6}
+SET2={3, 4, 5, 6, 7, 8, 9}
+print(f"Union with 'SET1' and 'SET2': {SET1.union(SET2)}")
+print(f"Intersection with 'SET1' and 'SET2': {SET1.intersection(SET2)}")
+print(f"Difference with 'SET1' and 'SET2': {SET1.difference(SET2)}")
+print(f"Difference with 'SET2' and 'SET1': {SET2.difference(SET1)}")'''
+#Practice Program-4: Adding to Sets
+'''SET1={1, 2, 3, 4, 5}
+SET1.add("red")
+print(SET1)'''
