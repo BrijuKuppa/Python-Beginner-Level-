ad83cf1d304cbfd48d8d402a0d231faf2b971588
rajgo <deepikamano20@gmail.com>
2024-03-10 13:02:23 -0400

    Homework

---
+++ Python-Intermediate-Level/Class 16 Homework.py
@@ -0,0 +1,24 @@
+#Practice Program-1
+'''class Fruit:
+    def __init__(self,color):
+        self.color=color
+        print(color)
+
+apple=Fruit("red")
+orange=Fruit("orange")'''
+#Practice Program-2
+'''class Employee:
+    def __init__(self,employee_name,employee_id,employee_experience):
+        self.employee_name=employee_name
+        self.employee_id=employee_id
+        self.employee_experience=employee_experience
+    def display(self):
+        print(f"Employee name: {self.employee_name}")
+        print(f"Employee ID: {self.employee_id}")
+        print(f"Employee experience: {self.employee_experience}")
+        print()
+
+ob1=Employee("Caryn",143746262,"10 years")
+ob2=Employee("Kate",51426322,"3 years")
+ob1.display()
+ob2.display()'''
