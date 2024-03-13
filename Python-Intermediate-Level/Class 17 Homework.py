a68a947c5f46817a811d2210b8d648c3f21b2da6
rajgo <deepikamano20@gmail.com>
2024-03-13 19:37:11 -0400

    Homework

--- Python-Intermediate-Level/Class 17 Homework.py
+++ Python-Intermediate-Level/Class 17 Homework.py
@@ -17,3 +17,17 @@ print(f"Difference with 'SET2' and 'SET1': {SET2.difference(SET1)}")'''
 '''SET1={1, 2, 3, 4, 5}
 SET1.add("red")
 print(SET1)'''
+#Practice Program-5: Flower Class
+'''class Flower:
+    def __init__(self,petals,stem,leaf,expression):
+        self.petals=petals
+        self.stem=stem
+        self.leaf=leaf
+        self.expression=expression
+    def emotion(self):
+        print(f"Flower is {self.expression}")
+
+Flower_ob1=Flower(6,1,2,"happy")
+Flower_ob2=Flower(6,1,2,"sad")
+Flower_ob1.emotion()
+Flower_ob2.emotion()'''
